0fbddb313a8304b6aaa8022057e8db4585a7059f
rohan <rohan.anakin@gmail.com>
2024-10-13 01:24:03 +1300

    feat: setup basic train movement

--- Scripts/Train.cs
+++ Scripts/Train.cs
@@ -6,10 +6,16 @@ public partial class Train : RigidBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//LinearVelocity = LinearVelocity with { X = 1f };
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
+    {
+        ApplyCentralForce(new Vector3(4,0,0));
+		
+    }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 }
