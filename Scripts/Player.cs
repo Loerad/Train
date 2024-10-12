fd18ca1eacc24a003efc272f19423f6387c17cae
rohan <rohan.anakin@gmail.com>
2024-10-12 22:10:37 +1300

    fix: look angle

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -18,21 +18,22 @@ public partial class Player : CharacterBody3D
 
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("Quit"))
+		if (Input.IsActionJustPressed("Quit")) //change to activate the pause menu
 		{
 			GetTree().Quit();
 		}
 	}
-
+	//handles mouse movement
 	public override void _Input(InputEvent @event)
     {
 		if (@event is InputEventMouseMotion mouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured)
 		{
 			body.RotateY(-mouseMotion.Relative.X * lookSensitivity);
 			cam.RotateX(-mouseMotion.Relative.Y * lookSensitivity);
-			cam.Rotation = Rotation with { X = Math.Clamp(cam.Rotation.X, Mathf.DegToRad(-80) , Mathf.DegToRad(80)) }; //https://docs.godotengine.org/en/stable/tutorials/scripting/c_sharp/c_sharp_basics.html#common-pitfalls
+			cam.Rotation = Rotation with { X = Math.Clamp(cam.Rotation.X, Mathf.DegToRad(-66.5f) , Mathf.DegToRad(63.4f)) }; //https://docs.godotengine.org/en/stable/tutorials/scripting/c_sharp/c_sharp_basics.html#common-pitfalls
 		}
     }
+	//base godot movement script
     public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
@@ -52,7 +53,7 @@ public partial class Player : CharacterBody3D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backwards");
-		Vector3 direction = (body.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		Vector3 direction = (body.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized(); //apart from acting on the mesh insead of the origin
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * SPEED;
